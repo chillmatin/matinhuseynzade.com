d2c7a8b51faa2f53f1617e8ee32946c87cce49a6
Matin Huseynzade <matinhuseynzade@gmail.com>
2025-02-06 11:00:56 +0300

    yolla artik rrr

---
+++ src/config.js
@@ -0,0 +1,17 @@
+export const metaData = {
+  baseUrl: "https://www.chilldude.me/",
+  title: "chilldude.me",
+  name: "Matin",
+  ogImage: "/opengraph-image.png",
+  description:
+    "A clean, fast, and lightweight portfolio template built with Next.js, Vercel, and Tailwind CSS for optimal performance.",
+};
+
+export const socialLinks = {
+  // twitter: "https://x.com/chillmatin",
+  github: "https://github.com/chillmatin",
+  instagram: "https://www.instagram.com/",
+  linkedin: "https://www.linkedin.com/in/chillmatin/",
+  email: "mailto:matinhuseynzade@gmail.com",
+  youtube: "https://www.youtube.com/@chilldude1337"
+};
