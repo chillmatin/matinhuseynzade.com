587c82f2051b1bf83ae8f6cf414d1a6f3ec0fbe3
chillmatin <matinhuseynzade@gmail.com>
2025-02-09 01:03:40 +0300

    added instagram

--- src/config.js
+++ src/config.js
@@ -10,7 +10,7 @@ export const metaData = {
 export const socialLinks = {
   // twitter: "https://x.com/chillmatin",
   github: "https://github.com/chillmatin",
-  instagram: "https://www.instagram.com/",
+  instagram: "https://www.instagram.com/chilldude.me",
   linkedin: "https://www.linkedin.com/in/chillmatin/",
   email: "mailto:matinhuseynzade@gmail.com",
   youtube: "https://www.youtube.com/@chilldude1337"
