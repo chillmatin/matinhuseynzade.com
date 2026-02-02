ce521dce32747aecac17501c970706c990e070b7
Matin Huseynzade <matinhuseynzade@gmail.com>
2026-02-03 00:21:40 +0400

    style: Website preview style change to `rauchg`
    [Source](https://www.npmjs.com/package/astro-opengraph-images) Further themes can be seen here

--- astro.config.mjs
+++ astro.config.mjs
@@ -58,7 +58,7 @@ export default defineConfig({
     }),
     sitemap(),
     opengraphImages({
-      render: presets.waveSvg,
+      render: presets.rauchg,
       options: {
         fonts: [
           {
